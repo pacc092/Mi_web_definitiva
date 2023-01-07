d2527402e3e1269ff3c7a173015085568feaf0d8
pacc092 <paulalejandro.clavoncastillo@mengibar.eu>
2023-01-07 20:57:11 +0100

    agregado script
    cambio en el scrip del formulario, además de mejora en el slider, falta aun campos por rellenar del formulario

--- js/productos.js
+++ js/productos.js
@@ -24,3 +24,20 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+function comprobar(){
+
+  if (document.miformulario.nombre.value =='' || document.miformulario.nombre.value.length <= 4) {
+    alert('No has rellenado el campo nombre');
+    alert('Este campo debe contener mínimo 4 caracteres')
+  } else {
+    document.form[0].submit();
+  }
+
+  if (document.miformulario.apellidos.value == '' || document.miformulario.value.length <=4) {
+    alert('No has rellenado el campo apellidos');
+    alert('Este campo debe contener mínimo 4 caracteres');
+  } else {
+    document.form[1].submit();
+  }
+}
