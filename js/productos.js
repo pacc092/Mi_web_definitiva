a63da1591bbd76af874fb472ec96373ea108eeda
pacc092 <paulalejandro.clavoncastillo@mengibar.eu>
2023-01-08 17:16:34 +0100

    cambios del formulario
    modificación de la validación del formulario

--- js/productos.js
+++ js/productos.js
@@ -26,18 +26,63 @@ function showSlides(n) {
 }
 
 function comprobar(){
-
+  /*Nombre*/
   if (document.miformulario.nombre.value =='' || document.miformulario.nombre.value.length <= 4) {
     alert('No has rellenado el campo nombre');
     alert('Este campo debe contener mínimo 4 caracteres')
   } else {
     document.form[0].submit();
   }
-
+ /*Apellidos*/
   if (document.miformulario.apellidos.value == '' || document.miformulario.value.length <=4) {
     alert('No has rellenado el campo apellidos');
     alert('Este campo debe contener mínimo 4 caracteres');
   } else {
     document.form[1].submit();
   }
+  /*Email*/
+  if (document.miformulario.email =='') {
+    alert('El correo está vacío');
+    
+  }else{
+    var email=document.miformulario.email.value;
+    var expreg=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+
+      if (!(expreg.test(email))) {
+        alert("El email es incorrecto");
+        
+      }else{
+        document.form[4].submit();
+      }
+  }
+  /*Sugerencias*/
+  if (document.miformulario.sugerencias.value =='') {
+    alert('Las sugerencias están vacías, introduzca texto');
+    
+  }else{
+
+    if (document.miformulario.sugerencias.value.length <=400);
+    document.form[12].submit();
+  }
+  /*dni*/
+
+  if (document.miformulario.dni.value =='') {
+    alert('Introduzca su DNI')
+    
+  } else {
+
+    var dni = document.miformulario.dni.value;
+    var expreg = /^\d{8}[A-Z]$/;
+
+    if (!(expreg.test(dni))) {
+        alert('El DNI es incorrecto');
+      
+    }else{
+      document.form[5].submit();
+    }
+    
+  }
+
+  
 }
+
